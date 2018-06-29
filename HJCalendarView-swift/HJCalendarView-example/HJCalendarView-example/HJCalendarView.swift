20604df8e5c101512d85ecb7265c1fe041506305
Hyun-je <bigae2@gmail.com>
2018-06-30 00:01:41 +0900

    second commit

--- HJCalendarView-swift/HJCalendarView-example/HJCalendarView-example/HJCalendarView.swift
+++ HJCalendarView-swift/HJCalendarView-example/HJCalendarView-example/HJCalendarView.swift
@@ -9,55 +9,122 @@
 import UIKit
 
 
-class HJCalendarViewCell: UICollectionViewCell {
-    
-    
-}
-
-
-
 
 class HJCalendarView: UICollectionView, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    
+    let stringWeek = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"]
+    
+
+    
+    var dayHeaderColor = UIColor.gray
+    var dateColor = UIColor.black
     
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+        
 
+        // set view
+        isPagingEnabled = true
+        
+        showsVerticalScrollIndicator = false
+        showsHorizontalScrollIndicator = false
+        
+        clipsToBounds = true
+        
+   
+        // set layout
         let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = 0.0
+        layout.minimumInteritemSpacing = 0.0
+        
         self.collectionViewLayout = layout
 
+
+        // register cell
         self.register(HJCalendarViewCell.self, forCellWithReuseIdentifier: "HJCalendarViewCell")
         
+
+
         delegate = self
         dataSource = self
+
         
     }
     
+    
+    override func willMove(toWindow newWindow: UIWindow?) {
+        super.willMove(toWindow: newWindow)
+        
+        if newWindow == nil {
+            // UIView disappear
+            
+            
+        } else {
+            // UIView appear
+            
+            let indexPath = IndexPath(row: 0, section: 1)
+            self.scrollToItem(at: indexPath, at: UICollectionViewScrollPosition.left, animated: false)
+            
+        }
+    }
+    
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        
+        let indexPath = IndexPath(row: 0, section: 1)
+        scrollToItem(at: indexPath, at: UICollectionViewScrollPosition.left, animated: false)
+        
+        
+    }
 
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-
-        return CGSize(width: 10, height: 10)
+    func collectionViewIndexTransform(index:Int) -> Int {
+        
+        let row = index/7
+        let col = index%7
+        
+        return col*7 + row
         
     }
     
     
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-
-        return 1
+        return 3
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-
         return 49
     }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        // 7 x 7 array
+        return CGSize(width: self.frame.width/7, height: self.frame.height/7)
+        
+    }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
 
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "HJCalendarViewCell", for: indexPath)
-        cell.backgroundColor = UIColor.black
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "HJCalendarViewCell", for: indexPath) as! HJCalendarViewCell
+        
+        let cellIndex = collectionViewIndexTransform(index:indexPath.row) - 7
+
+        if cellIndex < 0 {
+            // 상단 주 표시 텍스트
+            cell.mainLabel.text = stringWeek[cellIndex+7]
+            //cell.labelMain.textColor = UIColor(red: 0.207843, green: 0.098039, blue: 0.243137, alpha: 1.0)
+            cell.setCellType(.DayHeaderCell)
+            
+        }
+        else {
+            cell.mainLabel.text = "10"
+            cell.setCellType(.DateCell)
+        }
+
+
         
         return cell
         
---
+++ HJCalendarView-swift/HJCalendarView-example/HJCalendarView-example/HJCalendarViewCell.swift
@@ -0,0 +1,87 @@
+//
+//  HJCalendarViewCell.swift
+//  HJCalendarView-example
+//
+//  Created by JaehyeonPark on 2018. 6. 29..
+//  Copyright © 2018년 JaehyeonPark. All rights reserved.
+//
+
+import UIKit
+
+
+enum HJCalendarViewCellType {
+    
+    case DayHeaderCell
+    case DateCell
+    
+}
+
+
+class HJCalendarViewCell: UICollectionViewCell {
+    
+    var mainLabel = UILabel()
+    var countLabel = UILabel()
+    
+    
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+    }
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        mainLabel = UILabel(frame: CGRect(x: 0, y: 0, width: frame.width, height: frame.height/2))
+        mainLabel.center = CGPoint(x:frame.width/2 , y: frame.height/3)
+        mainLabel.textAlignment = .center
+        mainLabel.text = "10"
+        mainLabel.font = UIFont.systemFont(ofSize: frame.height/2)
+        addSubview(mainLabel)
+        
+        countLabel = UILabel(frame: CGRect(x: 0, y: 0, width: frame.width, height: frame.height*3/4))
+        countLabel.center = CGPoint(x:frame.width/2 , y: frame.height*3/4)
+        countLabel.textAlignment = .center
+        countLabel.text = "•••"
+        countLabel.font = UIFont.systemFont(ofSize: frame.height/5)
+        addSubview(countLabel)
+        
+    }
+    
+    
+    
+    func setCellType(_ type:HJCalendarViewCellType) {
+        
+        switch type {
+            
+        case .DayHeaderCell:
+            mainLabel.center = CGPoint(x:frame.width/2 , y: frame.height/2)
+            mainLabel.font = UIFont.systemFont(ofSize: frame.height/4)
+            countLabel.isHidden = true
+            
+        case .DateCell:
+            mainLabel.center = CGPoint(x:frame.width/2 , y: frame.height/3)
+            mainLabel.font = UIFont.systemFont(ofSize: frame.height/2)
+            countLabel.isHidden = false
+            
+        }
+        
+    }
+    
+    func setCountIcon(_ count:Int) {
+        
+        var countString = ""
+        
+        for i in 0..<count {
+            countString += "•"
+            if i >= 2 { break }
+        }
+        
+        countLabel.text = countString
+        
+    }
+    
+    
+    
+}
