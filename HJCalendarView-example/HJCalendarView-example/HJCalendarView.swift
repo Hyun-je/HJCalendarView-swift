3ce765341b3f184a0f072cc7fe35f094ff10951c
Hyun-je <bigae2@gmail.com>
2018-07-01 11:39:52 +0900

    separate HJCalendar class

--- HJCalendarView-example/HJCalendarView-example/HJCalendarView.swift
+++ HJCalendarView-example/HJCalendarView-example/HJCalendarView.swift
@@ -11,69 +11,6 @@ import UIKit
 
 
 
-class MonthCell {
-    
-    static let calendar = Calendar(identifier: .gregorian)
-    var date = Date()
-    
-    var calendarCount = [Int](repeating: 0, count:31)
-    
-    
-    init() {
-        let comp: DateComponents = MonthCell.calendar.dateComponents([.year, .month], from: Calendar.current.startOfDay(for: date))
-        if let date = MonthCell.calendar.date(from: comp) {
-            self.date = date
-        }
-    }
-    
-    init(year: Int, month: Int) {
-        let comp = DateComponents(year: year, month: month)
-        if let date = MonthCell.calendar.date(from: comp) {
-            self.date = date
-        }
-    }
-    
-    func getYear() -> Int? {
-        let comp: DateComponents = MonthCell.calendar.dateComponents([.year], from: date as Date)
-        return comp.year
-    }
-    
-    func getMonth() -> Int? {
-        let comp: DateComponents = MonthCell.calendar.dateComponents([.month], from: date as Date)
-        return comp.month
-    }
-    
-    func getNumberOfDay() -> Int {
-        let range = MonthCell.calendar.range(of: .day, in: .month, for: date)!
-        return range.count
-        
-    }
-    
-    func getWeekOfFirstDay() -> Int {
-        return MonthCell.calendar.dateComponents([.weekday], from: date).weekday! - 1
-    }
-
-    func setNextMonth() {
-        var periodComponents = DateComponents()
-        periodComponents.month = 1
-        date = MonthCell.calendar.date(byAdding: periodComponents, to: date)!
-    }
-    
-    func setPreviousMonth() {
-        var periodComponents = DateComponents()
-        periodComponents.month = -1
-        date = MonthCell.calendar.date(byAdding: periodComponents, to: date)!
-    }
-    
-    
-    
-}
-
-
-
-
-
-
 protocol HJCalendarViewDelegate: NSObjectProtocol {
     
     func didChangeCalendar(_ calendarView: HJCalendarView, year: Int, month: Int)
@@ -84,12 +21,13 @@ protocol HJCalendarViewDelegate: NSObjectProtocol {
 
 
 
+
 class HJCalendarView: UICollectionView {
     
     weak var calendarDelegate: HJCalendarViewDelegate?
     
     private let stringWeek = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"]
-    private var monthCellArray = [MonthCell]()
+    private var calendarArray = [HJCalendar]()
 
     
     @IBInspectable var dayHeaderColor = UIColor.gray
@@ -125,13 +63,13 @@ class HJCalendarView: UICollectionView {
         self.register(HJCalendarViewCell.self, forCellWithReuseIdentifier: "HJCalendarViewCell")
         
         
-        // init monthcell
+        // init HJCalendar array
         for _ in 0..<3 {
-            monthCellArray.append(MonthCell())
+            calendarArray.append(HJCalendar())
         }
         
-        monthCellArray[0].setPreviousMonth()
-        monthCellArray[2].setNextMonth()
+        calendarArray[0].setPreviousMonth()
+        calendarArray[2].setNextMonth()
         
         delegate = self
         dataSource = self
@@ -144,17 +82,17 @@ class HJCalendarView: UICollectionView {
     func setCurrentCalendar(year: Int, month: Int) {
 
         for i in 0..<3 {
-            monthCellArray[i] = MonthCell(year: year, month: month)
+            calendarArray[i] = HJCalendar(year: year, month: month)
         }
         
-        monthCellArray[0].setPreviousMonth()
-        monthCellArray[2].setNextMonth()
+        calendarArray[0].setPreviousMonth()
+        calendarArray[2].setNextMonth()
         
     }
     
     func getCurrentCalendar() -> (year: Int?, month: Int?) {
 
-        return (monthCellArray[1].getYear(), monthCellArray[1].getMonth())
+        return (calendarArray[1].getYear(), calendarArray[1].getMonth())
         
     }
 
@@ -188,7 +126,7 @@ extension HJCalendarView: UICollectionViewDelegateFlowLayout {
             let indexPath = IndexPath(row: 0, section: 1)
             self.scrollToItem(at: indexPath, at: UICollectionViewScrollPosition.left, animated: false)
             
-            calendarDelegate?.didChangeCalendar(self, year: monthCellArray[1].getYear()!, month: monthCellArray[1].getMonth()!)
+            calendarDelegate?.didChangeCalendar(self, year: calendarArray[1].getYear()!, month: calendarArray[1].getMonth()!)
 
             
         }
@@ -206,16 +144,16 @@ extension HJCalendarView: UICollectionViewDelegateFlowLayout {
                 
                 switch scrollDirection {
                 case 1:
-                    monthCellArray[i].setPreviousMonth()
+                    calendarArray[i].setPreviousMonth()
                 case -1:
-                    monthCellArray[i].setNextMonth()
+                    calendarArray[i].setNextMonth()
                 default:
                     break
                 }
                 
             }
             
-            calendarDelegate?.didChangeCalendar(self, year: monthCellArray[1].getYear()!, month: monthCellArray[1].getMonth()!)
+            calendarDelegate?.didChangeCalendar(self, year: calendarArray[1].getYear()!, month: calendarArray[1].getMonth()!)
             
         }
         
@@ -230,15 +168,15 @@ extension HJCalendarView: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
 
         let cellIndex = collectionViewIndexTransform(index:indexPath.row)
-        let dateIndex = cellIndex - monthCellArray[indexPath.section].getWeekOfFirstDay()
+        let dateIndex = cellIndex - calendarArray[indexPath.section].getWeekOfFirstDay()
         
         if cellIndex < 0 {
             calendarDelegate?.didSelectDay(self, indexPath: indexPath, date: nil)
         }
-        if dateIndex >= 0 && dateIndex < monthCellArray[indexPath.section].getNumberOfDay() {
+        if dateIndex >= 0 && dateIndex < calendarArray[indexPath.section].getNumberOfDay() {
             
-            let comp = DateComponents(year: monthCellArray[1].getYear(), month: monthCellArray[1].getMonth(), day: dateIndex + 1)
-            calendarDelegate?.didSelectDay(self, indexPath: indexPath, date: MonthCell.calendar.date(from: comp))
+            let comp = DateComponents(year: calendarArray[1].getYear(), month: calendarArray[1].getMonth(), day: dateIndex + 1)
+            calendarDelegate?.didSelectDay(self, indexPath: indexPath, date: HJCalendar.calendar.date(from: comp))
             
         }
         else {
@@ -288,7 +226,7 @@ extension HJCalendarView: UICollectionViewDataSource {
         cell.indexPath = indexPath
         
         let cellIndex = collectionViewIndexTransform(index:indexPath.row)
-        let dateIndex = cellIndex - monthCellArray[indexPath.section].getWeekOfFirstDay()
+        let dateIndex = cellIndex - calendarArray[indexPath.section].getWeekOfFirstDay()
 
         
         if cellIndex < 0 {
@@ -297,14 +235,14 @@ extension HJCalendarView: UICollectionViewDataSource {
             cell.mainLabel.textColor = dayHeaderColor
             cell.setCellType(.DayHeaderCell)
         }
-        else if dateIndex >= 0 && dateIndex < monthCellArray[indexPath.section].getNumberOfDay() {
+        else if dateIndex >= 0 && dateIndex < calendarArray[indexPath.section].getNumberOfDay() {
             // 날짜 표시 텍스트
             cell.mainLabel.textColor = dateColor
             cell.setCellType(.DateCell)
             cell.mainLabel.text = "\(dateIndex + 1)"
             
-            let comp = DateComponents(year: monthCellArray[1].getYear(), month: monthCellArray[1].getMonth(), day: dateIndex + 1)
-            cell.date = MonthCell.calendar.date(from: comp)
+            let comp = DateComponents(year: calendarArray[1].getYear(), month: calendarArray[1].getMonth(), day: dateIndex + 1)
+            cell.date = HJCalendar.calendar.date(from: comp)
         }
         else {
             // 빈킨 표시 텍스트
